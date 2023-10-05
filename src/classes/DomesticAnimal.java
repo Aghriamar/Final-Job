44053f7aeec97edc8cf4aca42807c9a8b03b9cc8
Aghriamar <A1exXx88@inbox.ru>
2023-10-05 18:13:38 +0300

    Documentation class DomesticAnimal

--- src/classes/DomesticAnimal.java
+++ src/classes/DomesticAnimal.java
@@ -1,6 +1,17 @@
 package classes;
 
+/**
+ * Класс представляет домашнее животное.
+ * Этот класс наследует базовые характеристики животного из класса Animal.
+ */
 public class DomesticAnimal extends Animal {
+    /**
+     * Конструктор для создания домашнего животного.
+     *
+     * @param name    Имя животного.
+     * @param type    Тип животного (домашнее или вьючное).
+     * @param command Команда, которую выполняет животное.
+     */
     public DomesticAnimal(String name, String type, String command) {
         super(name, type, command);
     }
