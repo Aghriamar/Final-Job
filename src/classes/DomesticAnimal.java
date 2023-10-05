3cd4764d28d217dc2c8137837ac45fe08089c338
Aghriamar <A1exXx88@inbox.ru>
2023-10-05 18:13:07 +0300

    Added class DomesticAnimal

---
+++ src/classes/DomesticAnimal.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class DomesticAnimal extends Animal {
+    public DomesticAnimal(String name, String type, String command) {
+        super(name, type, command);
+    }
+}
