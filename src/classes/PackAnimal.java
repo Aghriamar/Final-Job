46cfe7966495be2a54db98ac1959e3030f4966c1
Aghriamar <A1exXx88@inbox.ru>
2023-10-05 18:12:18 +0300

    Documentation class PackAnimal

--- src/classes/PackAnimal.java
+++ src/classes/PackAnimal.java
@@ -1,6 +1,17 @@
 package classes;
 
+/**
+ * Класс представляет вьючное животное.
+ * Этот класс наследует базовые характеристики животного из класса Animal.
+ */
 public class PackAnimal extends Animal {
+    /**
+     * Конструктор для создания вьючного животного.
+     *
+     * @param name    Имя животного.
+     * @param type    Тип животного (домашнее или вьючное).
+     * @param command Команда, которую выполняет животное.
+     */
     public PackAnimal(String name, String type, String command) {
         super(name, type, command);
     }
